e47f4561dcd5356343ab4a5485ad87b3719a0673
joaovic499 <joaovictorsilva60@live.com>
2024-04-26 11:50:32 -0300

    projeto bugado

--- src/app/shared/element-dialog/element-dialog.component.ts
+++ src/app/shared/element-dialog/element-dialog.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { PeriodicElement } from '../../views/home/home.component';
-import {MAT_DIALOG_DATA, MatDialogRef,} from '@angular/material/dialog';
-
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { PeriodicElement } from 'src/app/models/PeriodicElement';
 @Component({
   selector: 'app-element-dialog',
   templateUrl: './element-dialog.component.html',
-  styleUrl: './element-dialog.component.scss'
+  styleUrls: ['./element-dialog.component.scss']
 })
-export class ElementDialogComponent implements OnInit{
+export class ElementDialogComponent implements OnInit {
   element!: PeriodicElement;
+  isChange!: boolean;
 
   constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: PeriodicElement,
     public dialogRef: MatDialogRef<ElementDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: PeriodicElement,
   ) {}
 
-    ngOnInit(): void {
-  }
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+  ngOnInit(): void {
+    if (this.data.position != null) {
+      this.isChange = true;
+    } else {
+      this.isChange = false;
+    }
   }
 
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+}
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,36 +1,82 @@
-import { Component } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTable } from '@angular/material/table';
+import { PeriodicElement } from 'src/app/models/PeriodicElement';
+import { PeriodicElementService } from 'src/app/services/periodicElement.service';
+import { ElementDialogComponent } from 'src/app/shared/element-dialog/element-dialog.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
+  providers: [PeriodicElementService]
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  @ViewChild(MatTable)
+  table!: MatTable<any>;
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'actions'];
-  dataSource = ELEMENT_DATA;
+  dataSource!: PeriodicElement[];
+
+  constructor(
+    public dialog: MatDialog,
+    public periodicElementService: PeriodicElementService
+    ) {
+      this.periodicElementService.getElements()
+        .subscribe((data: PeriodicElement[]) => {
+          console.log(data);
+          this.dataSource = data;
+        });
+    }
+
+  ngOnInit(): void {
+  }
+
+  openDialog(element: PeriodicElement | null): void {
+    const dialogRef = this.dialog.open(ElementDialogComponent, {
+      width: '250px',
+      data: element === null ? {
+        position: null,
+        name: '',
+        weight: null,
+        symbol: ''
+      } : {
+        id: element.id,
+        position: element.position,
+        name: element.name,
+        weight: element.weight,
+        symbol: element.symbol
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result !== undefined) {
+        console.log(result);
+        if (this.dataSource.map(p => p.id).includes(result.id)) {
+          this.periodicElementService.editElement(result)
+            .subscribe((data: PeriodicElement) => {
+              const index = this.dataSource.findIndex(p => p.id === data.id);
+              this.dataSource[index] = data;
+              this.table.renderRows();
+            });
+        } else {
+          this.periodicElementService.createElements(result)
+            .subscribe((data: PeriodicElement) => {
+              this.dataSource.push(data);
+              this.table.renderRows();
+            });
+        }
+      }
+    });
+  }
+
+  editElement(element: PeriodicElement): void {
+    this.openDialog(element);
+  }
+
+  deleteElement(position: number): void {
+    this.periodicElementService.deleteElement(position)
+      .subscribe(() => {
+        this.dataSource = this.dataSource.filter(p => p.id !== position);
+      });
+  }
 }
-
-
